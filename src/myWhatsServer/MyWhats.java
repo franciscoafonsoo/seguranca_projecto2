6c27617ac7ebe3ff51bdc4cd92bf499a9fef6917
Pedro <pneves6@hotmail.com>
2016-03-05 18:07:30 +0000

    message sent to server

--- src/myWhatsServer/MyWhats.java
+++ src/myWhatsServer/MyWhats.java
@@ -9,6 +9,8 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class MyWhats {
@@ -19,6 +21,10 @@ public class MyWhats {
 		String[] server = args[2].split(":");
 		String IP = server[0];
 		int port = Integer.parseInt(server[1]);
+		List<String> argv = null;
+		for (String e : args) {
+			argv.add(e);
+		}
         Socket s = new Socket(IP, port);
         ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(s.getInputStream());
@@ -32,6 +38,9 @@ public class MyWhats {
         	out.writeObject(user);
         	String ack = (String) in.readObject();
         	if (ack.equals("NOK")) {throw new BadPwdException("wrong password!");}
+        	argv.remove(1); argv.remove(1); argv.remove(1); argv.remove(1);
+        	handle(argv, out);
+        	
         }
         
         else {
@@ -41,7 +50,12 @@ public class MyWhats {
         	out.writeObject(passwd);
         	String ack = (String) in.readObject();
         	if (ack.equals("NOK")) {throw new BadPwdException("wrong password!");}
+        	argv.remove(1); argv.remove(1);
+        	handle(argv, out);
         }
+        
+        
+        
         
         File f = new File("IIO-Exame_2014_01_20.pdf");
         FileInputStream input = new FileInputStream (f);
@@ -51,5 +65,29 @@ public class MyWhats {
        while ((n = input.read(buf, 0, 32))!=-1) {
     	   out.write(buf, 0, n);
        }
+       
     }
+	
+	private static void handle(List<String> lista, ObjectOutputStream out) throws IOException {
+		String[] args = (String[]) lista.toArray();
+		if (args[0] == "-r"){
+			String mensagem = null;
+			if (args.length==1) {
+				mensagem="-r";
+			}
+			else if (args.length==2) {
+				mensagem = args[0] + ":"+ args[1];
+			}
+			else {
+				mensagem = args[0] + ":"+ args[1] +":"+ args[2];
+			}
+			out.writeObject(mensagem);
+		}
+		else {
+			String mensagem2 = args[0] + ":"+ args[1] +":"+ args[2];
+			out.writeObject(mensagem2);
+		}
+		
+		}
+			
 }
