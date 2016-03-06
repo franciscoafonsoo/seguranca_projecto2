f25c0548621a49da72cc53ee9368802221879e8b
sherby <sherby@bitbucket.org>
2016-03-06 15:49:17 +0000

    skel

---
+++ src/myWhatsServer/MyWhatsSkel.java
@@ -0,0 +1,60 @@
+package myWhatsServer;
+
+/**
+ * Created by sherby on 06-03-2016.
+ */
+public class MyWhatsSkel {
+
+	public MyWhatsSkel {
+
+	}
+
+	/**
+	 * autenticar um cliente "user"
+	 *
+	 */
+
+	public void auth(String user, String pass) {
+
+	}
+
+	/**
+	 * registar um client "user"
+	 *
+	 */
+
+	public void register(String user, String pass) {
+
+	}
+
+	/**
+	 * opcao -m
+	 * recebe mensagem e da autorizacao de acesso ao client "user"
+	 *
+	 */
+
+	public void receiveMessage(String msg, String user) {
+
+	}
+
+	/**
+	 * opcao -f
+	 *
+	 * recebe um ficheiro no servidor e da autorizacao de acesso ao client "user"
+	 *
+	 */
+
+	public void receiveFile(File f, String user) {
+
+	}
+
+	/**
+	 * opcao -r 
+	 *
+	 * partilha o nome ficheiro/mensagem trocada por outro client "user"
+	 *
+	 */
+
+	public void shareMessage(File f, String user, String)
+
+}
